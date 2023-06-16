1c77e735fd26ca6610f6ded3720b16a9a9062d6b
Lola García <mariadolores.garcia@bosonit.com>
2023-06-16 09:13:05 +0200

    output exercise done

--- 00-angularada/src/app/app.component.ts
+++ 00-angularada/src/app/app.component.ts
@@ -7,14 +7,41 @@ import { Persona } from './interface/persona';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'angularada';
 
+  //mensaje del padre mostrado por interpolacion {{}} en el propio padre
   mensajePadre:string = 'Este es el mensaje del componente PADRE que tenemos en el archivo app.component.ts ';
 
+  //mensaje  vacío que rellenaremos en el padre con datos recibidos del hijo  al hacer click 
+  mensajeRecibido:string = '';
+
+  //objeto vacío que rellenaremos  en el padre con datos recibidos del hijo al hacer click 
+  objetoRecibido:Persona = {
+    nombre: '',
+    empresa: '',
+    antiguedad: 0
+  };
+
+  //objeto que el padre pasará al hijo
   datosPadre: Persona = {
     nombre: 'Lola',
     empresa: 'Bosonit',
     antiguedad: 1
   };
+
+  constructor(){    }  
+
+  //función para que el padre reciba mensaje string de parte del hijo
+  recibirMensaje(evento:string){
+    console.log(evento);
+    this.mensajeRecibido = evento;
+  } 
+
+  //función para que el padre reciba objeto Persona de parte del hijo
+  recibirObjeto(evento:Persona){
+    console.log(evento);
+    this.objetoRecibido = evento;
+  } 
   
 }
--- 00-angularada/src/app/hijo/hijo.component.ts
+++ 00-angularada/src/app/hijo/hijo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Persona } from '../interface/persona';
 
 @Component({
@@ -11,17 +11,36 @@ export class HijoComponent {
   
   mensajeHijoPadre:string = 'Texto que el componente HIJO envía al padre';
 
-  datosHijoPadre: Persona = {
-    nombre: 'Giovanni',
-    empresa: 'Bosonit',
-    antiguedad: 2
-  };
-
+  //INPUT para recibir el hijo los datos del padre.
+  //se trata de un objeto vacío 
   @Input() datosHijo: Persona = {
     nombre: '',
     empresa: '',
     antiguedad: 0
   };
 
+  //objeto con datos para enviar al padre 
+  datosHijoPadre: Persona = {
+    nombre: 'Giovanni',
+    empresa: 'Bosonit',
+    antiguedad: 2
+  };
+  
   constructor() { }
+
+  //OUTPUT para enviar al padre un evento de tipo string (un mensaje)
+  @Output() paraPadre = new EventEmitter<string>();
+
+  //función que creamos para enviar al padre este evento usando el método emit y como parámetro el string
+  enviarMensajePadre(){
+    this.paraPadre.emit(this.mensajeHijoPadre)
+  }
+
+  //OUTPUT para enviar al padre un evento de tipo Persona
+  @Output() objetoParaPadre = new EventEmitter<Persona>();
+
+  //función que creamos para enviar al padre este evento usando el método emit y como parámetro el objeto
+  enviarObjetoPadre(){
+    this.objetoParaPadre.emit(this.datosHijoPadre)
+  }
 }
