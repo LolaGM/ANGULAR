d8577e3c450b65588267883237ca5d607c43c369
Lola García <mariadolores.garcia@bosonit.com>
2023-07-13 09:17:05 +0200

    added map box key

--- 10-mapsApp/src/environments/environments.ts
+++ 10-mapsApp/src/environments/environments.ts
@@ -1,5 +1,5 @@
 
     export const environment = {
-        mapbox_key: "undefined" ,
+        mapbox_key: "pk.eyJ1IjoibG9sYWdtIiwiYSI6ImNsank3amVtbzAyZ2IzZnFyd2w4a3V1cmIifQ.AkvYvhMZOua3XNDA7I8W7w" ,
         otra: "PROPIEDAD",
     };
