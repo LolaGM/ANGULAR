b31ca2a94dd38db5722e71edd2ef62ba6d780ed2
Lola García <mariadolores.garcia@bosonit.com>
2023-07-20 10:39:24 +0200

    pending user info on dashboard

--- 13-authApp/src/app/app-routing.module.ts
+++ 13-authApp/src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })], //useHash pone un # en la ruta del navegador cuando hacemos desplegue a producción en netlify por ej
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- 13-authApp/src/environments/environments.ts
+++ 13-authApp/src/environments/environments.ts
@@ -2,5 +2,5 @@
 export const environment = {
     baseUrl: 'http://localhost:3000'  //baseUrl es el endpoint adonde quiero llegar
 
-    //baseUrl: 'https://nest-auth-backend.up.railway.app' que es donde se desplega a producción
+    //baseUrl: 'https://nest-auth-backend.up.railway.app' que es donde se desplega a producción si uso railway
 }
